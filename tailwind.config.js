d5c47c1eb9293e6e1fb9f0cb89c59ae08c71e71c
karanimaina <karani.maina2010@gmail.com>
2022-12-06 23:21:23 +0300

    configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [],
+  theme: {
+    colors: {'DEFAULT': '#00ADB5', 'black': '#222831', 'white': '#EEEEEE', 'gray': '#393E46'},
+    container: {
+      center: true,
+      padding: { DEFAULT: '1rem', sm: '2rem', lg: '4rem', xl: '5rem'},
+             }},
+  extend: {},
+  plugins: []
+}
