d677df1df851a8db8fd466748dc7ec3ae3fd92cf
karanimaina <karani.maina2010@gmail.com>
2022-12-19 18:06:10 +0300

    add  user and repo classe

--- src/app/repo/repository/repository.component.ts
+++ src/app/repo/repository/repository.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../model/user";
+import {DataService} from "../data.service";
 
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
   styleUrls: ['./repository.component.css']
 })
-export class RepositoryComponent {
+export class RepositoryComponent implements OnInit{
+  username!: User;
+  repository: any;
+
+  constructor(private requestUser:DataService) {}
+
+  getUser(githubUsername: string){
+    this.requestUser.getUserDataRequest(githubUsername).then(
+      (Response)=>{
+        this.username=this.requestUser.userData;
+        console.log(this.username)
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
+  }
+
+
+  getRepos(githubUsername: string){
+    this.requestUser.getReposDataRequest(githubUsername).then(
+      (Response)=>{
+        this.repository=this.requestUser.repoData;
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
+  }
+
+
+
+  ngOnInit(): void {
+    this.getUser("karanimaina")
+    this.getRepos("karanimaina")
+
+  }
+
+}
+
 
 }
